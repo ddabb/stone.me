98fa118fb583618a839bc12bc2fb8e0bd67770c9
ddabb <417144452@qq.com>
2018-12-28 22:15:49 +0800

    Site updated: 2018-12-28 22:15:48

--- photos/photo.js
+++ photos/photo.js
@@ -21,8 +21,8 @@ photo ={
            imageY = imageSize.split('.')[1];
             li += '<div class="card" style="width:330px">' +
                     '<div class="ImageInCard" style="height:'+ 330 * imageY / imageX + 'px">' +
-                      '<a data-fancybox="gallery" href="https://github.com/ddabb/ddabb.github.io/tree/master/photos/images/' + imgNameWithPattern + '?raw=true" data-caption="' + imgName + '">' +
-                        '<img src="https://github.com/ddabb/ddabb.github.io/tree/master/photos/images/' + imgNameWithPattern + '?raw=true"/>' +
+                      '<a data-fancybox="gallery" href="https://github.com/ddabb/ddabb.github.io/tree/master/photos/images/' + imgNameWithPattern + '" data-caption="' + imgName + '">' +
+                        '<img src="https://github.com/ddabb/ddabb.github.io/tree/master/photos/images/' + imgNameWithPattern + '"/>' +
                       '</a>' +
                     '</div>' +
                     // '<div class="TextInCard">' + imgName + '</div>' +
